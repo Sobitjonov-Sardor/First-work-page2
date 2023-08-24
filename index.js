2122680bbfb82be5cbfd14388a19ed6a7b0ada28
sobitjonovsardor <sobitjonovsardor07@gmail.com>
2023-08-24 11:40:35 +0300

    Add all files

---
+++ index.js
@@ -0,0 +1,28 @@
+const menuBtn = document.querySelector('.menu-btn');
+
+menuBtn.addEventListener('click', function(){
+  menuBtn.classList.toggle('open');
+});
+
+const indicator = document.querySelector('.lvl-indicator');
+const items = document.querySelectorAll('.lvl-item');
+
+function handleIndicator(el) {
+    items.forEach(item => {
+        item.classList.remove('is-active');
+        item.removeAttribute('style');
+    });
+
+    indicator.style.width = `${el.offsetWidth}px`;
+    indicator.style.left = `${el.offsetLeft}px`;
+    indicator.style.backgroundColor = el.getAttribute('active-color');
+
+    el.classList.add('is-active');
+    el.style.color = el.getAttribute('active-color');
+}
+
+
+items.forEach((item, index) => {
+    item.addEventListener('click', (e) => { handleIndicator(e.target) });
+    item.classList.contains('is-active') && handleIndicator(item);
+});
